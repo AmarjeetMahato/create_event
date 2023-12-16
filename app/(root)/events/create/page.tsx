35a950167b65df295755ec42255e684be0cc7b1d
amarjitmahato016@gmail.com <amarjitmahato016@gmail.com>
2023-12-16 21:05:57 +0530

    Create event page

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreateEvent = () => {
+  return (
+    <section className='bg-primary-50 bg-dotted-pattern bg-cover'>
+                   <h3 className='wrapper h3-bold text-centersm:text-left'>CreateEvent</h3>
+    </section>
+ 
+  )
+}
+
+export default CreateEvent
